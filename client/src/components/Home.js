e04081d053fede7389674f47e1fac9ee510704e2
Harmony Kunzler <hkunzler@gmail.com>
2019-03-20 10:55:51 -0600

    updates

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import { withProduct } from '../context/ProductProvider'
 import SearchForm from './SearchForm'
 class Home extends Component {
+    componentDidMount = () => {
+        this.props.getProduct()
+    }
     render() {
+        const mappedProduct = this.props.product.map(product => (
+            <div>
+                <h1>product.itemName</h1>
+            </div>
+        ))
         return (
             <div>
                 <h1>This is the Home Page.</h1>
-                <SearchForm/>
+                <SearchForm />
+                {mappedProduct}
             </div>
         );
     }
