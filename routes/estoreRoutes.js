02addbcf799b9e726e91739c1338790f9e97e2e4
Harmony Kunzler <hkunzler@gmail.com>
2019-03-21 15:56:39 -0600

    Connecting the front and back

--- routes/estoreRoutes.js
+++ routes/estoreRoutes.js
@@ -2,12 +2,22 @@ const express = require('express')
 const estoreRouter = express.Router()
 const Products = require('../models/product.js')
 
+estoreRouter.post('/', (req, res, next) => {
+    const newProduct = new Products(req.body)
+    newProduct.save((err, newSavedProduct) => {
+        if (err) {
+            return res.status(500).send(err)
+        }
+        return res.status(201).send(newSavedProduct)
+    })
+})
 estoreRouter.get("/" , (req, res) => {
     Products.find((err, products) => {
         if (err){
             res.status(500)
             return res.send(err)
         }
+        console.log('products')
         return res.status(200).send(products)
     })
 })
