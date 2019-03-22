91865bb38440453ad904649a90b387918a73102e
Harmony Kunzler <hkunzler@gmail.com>
2019-03-22 17:47:57 -0600

    connected to shopping cart and shopping cart logo

--- client/src/context/ProductProvider.js
+++ client/src/context/ProductProvider.js
@@ -7,7 +7,8 @@ class ProductProvider extends Component {
     constructor() {
         super()
         this.state = {
-            product: []
+            product: [],
+            cart: []
         }
     }
     handleChange = e => {
@@ -27,12 +28,23 @@ class ProductProvider extends Component {
             })
         }).catch(error => console.log(error))
     }
+    addToCart = (_id) => {
+        console.log(_id)
+        axios.get(`/products/${_id}`).then(response => {
+            console.log(response.data)
+            this.setState(prevState => ({
+                cart: [...prevState.cart, response.data]
+            }))
+        }).catch(error => console.log(error))
+    }
     render() {
         return (
             <div>
                 <ProductContext.Provider
                     value={{
                         product: this.state.product,
+                        cart: this.state.cart,
+                        addToCart: this.addToCart,
                         getProduct: this.getProduct,
                         handleDelete: this.handleDelete,
                         handleChange: this.handleChange,
