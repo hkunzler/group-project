0d5c2928ca82806bd5e6402005c68984aee19b42
Nick Philips <nicholasphilips622@gmail.com>
2019-03-24 19:31:01 -0600

    Nested Navbar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withProduct } from '../context/ProductProvider'
 import SearchForm from './SearchForm'
+import NestedNavbar from './NestedNavbar.js'
 import Cards from './Card'
 
 
@@ -11,6 +12,7 @@ class Home extends Component {
     render() {
         return (
             <div className="home-container">
+                <NestedNavbar/>
                 <Cards/>
             </div>
         );
