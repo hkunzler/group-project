31eb041e17a56009446619e00be0b3086a85d6b4
Harmony Kunzler <hkunzler@gmail.com>
2019-03-25 15:14:35 -0600

    made it so each item has it's own page

--- client/src/components/MenPants.js
+++ client/src/components/MenPants.js
@@ -4,7 +4,7 @@ import {
     CardTitle, CardSubtitle, Button
 } from 'reactstrap';
 import { withProduct } from '../context/ProductProvider'
-import { Carousel } from 'react-responsive-carousel';
+import { Link } from "react-router-dom";
 
 
 
@@ -17,8 +17,10 @@ class MenPants extends Component {
     render() {
         const mappedProduct = this.props.product.map(product => (
             < div  >
-                < div style={{margin: '10px 15px'}}>
+                < div style={{ margin: '10px 15px' }}>
+                    <Link to={product._id}>
                     <CardImg style={{ width: '100%', height: '500px'}} src={product.imgURL} alt={product.itemName} />
+                    </Link>
                 <CardBody >
                     <CardTitle>{product.itemName}</CardTitle>
                     <CardSubtitle>{product.description}</CardSubtitle>
