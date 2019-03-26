b4b76f08c6284884af924fc8ad5c06a2425706f2
Harmony Kunzler <hkunzler@gmail.com>
2019-03-25 22:57:18 -0600

    undid random checkout

--- client/src/constants/stripe.js
+++
@@ -1,5 +0,0 @@
-const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production' ?
-    'pk_live_ycE98SE2Hnrn2MQaLmrLdTxM00QMwOkTG4':
-    'pk_test_ycE98SE2Hnrn2MQaLmrLdTxM00QMwOkTG4';
-
-export default STRIPE_PUBLISHABLE;
